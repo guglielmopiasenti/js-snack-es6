dd6ad82400ce3b5b84a226f0d8aadee0b0adb890
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-13 15:42:02 +0200

    adding const vip table and names array

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("JS OK");
+
+// adding vip table variable
+const tableName = "VIP Table";
+
+// adding guestlist array
+const guestList = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
+
+// .map array method to add info about seatings
