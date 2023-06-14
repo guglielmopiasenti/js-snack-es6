71fc5fcf45a11258725dc7c9ffa4d2150e7205b5
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-14 14:49:57 +0200

    creating array of objects

---
+++ snack3.js
@@ -0,0 +1,43 @@
+// creating an array of objects
+const bikes = [
+  {
+    name: "Trek Domane SLR 9",
+    weight: 7.2,
+  },
+  {
+    name: "Specialized Roubaix Expert",
+    weight: 7.8,
+  },
+  {
+    name: "Cannondale SuperSix EVO Hi-MOD Disc",
+    weight: 7.1,
+  },
+  {
+    name: "Giant Defy Advanced Pro 1",
+    weight: 7.5,
+  },
+  {
+    name: "Pinarello Dogma F12",
+    weight: 6.9,
+  },
+  {
+    name: "Scott Addict RC Ultimate",
+    weight: 6.8,
+  },
+  {
+    name: "BMC Teammachine SLR01 Four",
+    weight: 7.3,
+  },
+  {
+    name: "Cervélo S5 Dura-Ace Di2",
+    weight: 7.6,
+  },
+  {
+    name: "Colnago C64",
+    weight: 7.4,
+  },
+  {
+    name: "Orbea Orca M21eLTD-D",
+    weight: 6.7,
+  },
+];
